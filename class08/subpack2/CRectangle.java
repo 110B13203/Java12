e0803136a539bdbe357ec1e5ddf8e27958ba084e
110B13203 <112997321+110B13203@users.noreply.github.com>
2022-12-04 22:31:57 +0800

    Delete class08 directory

--- class08/subpack2/CRectangle.java
+++
@@ -1,9 +0,0 @@
-// CRectangle.java, 此檔案置於pack6\subpack2資料夾內
-package class08.subpack2;    // 將CRectangle類別納入pack6.subpack2中
-public class CRectangle
-{
-   public void show()
-   {
-      System.out.println("show() in class CRectangle called");
-   }
-}
